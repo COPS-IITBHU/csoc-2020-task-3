4078c164ab1643504df37488f1ff08a4a5274d4c
Satendra <satendra.raj1241@gmail.com>
2020-05-15 10:21:00 +0530

    authentication compleated

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,23 +1,28 @@
 from rest_framework import serializers
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
-
+from rest_framework.authtoken.models import Token
 
 class TokenSerializer(serializers.Serializer):
     token = serializers.CharField(max_length=500)
 
 
 class LoginSerializer(serializers.Serializer):
-    # TODO: Implement login functionality
-    pass
-
+    username=serializers.CharField(max_length=100)
+    password=serializers.CharField(max_length=100)
 
 class RegisterSerializer(serializers.Serializer):
-    # TODO: Implement register functionality
-    pass
-
+    username=serializers.CharField(max_length=100)
+    password=serializers.CharField(max_length=100)
+    email=serializers.CharField(max_length=100)
+    first_name=serializers.CharField(max_length=100)
+    last_name=serializers.CharField(max_length=100)
+    password=serializers.CharField(max_length=100)
+    
 
 class UserSerializer(serializers.ModelSerializer):
     # TODO: Implement the functionality to display user details
-    pass
-    
+    name= serializers.CharField(source='get_full_name')
+    class Meta:
+        model=User
+        fields=['id','name','email','username']
